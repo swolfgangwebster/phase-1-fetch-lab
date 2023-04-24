f5419e69f0e7fd44aa7a36b49580c5d3b18e62d1
Shawn Webster <swolfgangwebster@gmail.com>
2023-04-23 20:02:26 -0500

    h

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(function(resp) { return resp.json()})
+  .then(function(data) {renderBooks(data)})
+
 }
 
 function renderBooks(books) {
